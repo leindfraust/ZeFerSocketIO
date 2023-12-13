9c56f34582da8a9e671448254f68570e8542e1b3
Leindfraust <rozer223@gmail.com>
2023-12-13 19:49:37 +0800

    feat: added notifications support and fixed room leave bug

--- index.ts
+++ index.ts
@@ -3,7 +3,10 @@ import { Server } from "socket.io";
 import type { Socket } from "socket.io";
 import socketAuthorization from './controllers/socketAuthorization';
 import { submitComment } from './controllers/socketComment';
-import { PostComment } from '@prisma/client';
+import type { PostComment } from '@prisma/client';
+import type { UserNotifications } from '@prisma/client';
+import { submitNotification } from './controllers/socketNotifications';
+import prisma from './db';
 
 const PORT = process.env.PORT || 5000;
 const env = process.env.NODE_ENV
@@ -15,19 +18,26 @@ const io = new Server(httpServer, {
     methods: ["GET", "POST"]
   }
 });
+let currentRoom: string
+let notifRoom: string
 
-function leaveAllRooms(socket: Socket) {
+async function leaveAllRooms(socket: Socket) {
   const rooms = Array.from(socket.rooms);
 
   // Skip the default room, which has the socket ID as its name
+  const checkRoomIfUserId = await prisma.user.findUnique({
+    where: { id: notifRoom ?? '' },
+    select: {
+      id: true
+    }
+  })
   rooms.forEach((room) => {
-    if (room !== socket.id) {
-      socket.leave(room);
+    if (room !== socket.id && room !== checkRoomIfUserId?.id && room !== currentRoom) {
+      socket.leave(room)
     }
   });
 }
 
-let currentRoom: string
 io.use((socket, next) => socketAuthorization(socket, next))
 io.on("connection", (socket: Socket) => {
 
@@ -37,10 +47,20 @@ io.on("connection", (socket: Socket) => {
     currentRoom = titleId
   })
 
+  socket.on('initializeSocketNotificationRoom', (userId: string) => {
+    leaveAllRooms(socket)
+    socket.join(userId)
+    notifRoom = userId
+  })
+
   socket.on('submitComment', async ({ titleId, userId, content, commentReplyPostId }: PostComment & { titleId: string, content: string, commentReplyPostId: string }) => {
     const rooms = Array.from(socket.rooms);
     if (!rooms.find(room => room === currentRoom)) await socket.join(currentRoom)
     await submitComment(socket.id, titleId, userId, content, io, currentRoom, commentReplyPostId)
+  })
+
+  socket.on('submitNotification', async ({ userId, from, fromImage, message, actionUrl }: UserNotifications) => {
+    await submitNotification(notifRoom, userId, from, fromImage, message, actionUrl, io)
   })
 });
 
